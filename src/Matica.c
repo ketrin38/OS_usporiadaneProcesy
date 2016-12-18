e88e2fbe9ea4453be47e383556e3c3bc9fcd6f74
Nightwalker495 <milan.ondrasovic@gmail.com>
2016-12-18 13:18:44 +0100

    Opraveny parser.

--- src/Matica.c
+++ src/Matica.c
@@ -1,14 +1,18 @@
 #include <stdlib.h>
+#include <string.h>
 
 #include "Matica.h"
+
+static void vycisti_pole(int **pole, size_t velkost);
 
 Matica* up_matica_vytvor(size_t velkost_strany) {
 	Matica *inst = (Matica *) malloc(sizeof(Matica));
 	inst->velkost_strany = velkost_strany;
 	inst->pole = (int **) malloc(velkost_strany * sizeof(int));
-	for (int i = 0; i < velkost_strany; i++) {
+	for (size_t i = 0; i < velkost_strany; i++) {
 		inst->pole[i] = (int *) malloc(velkost_strany * sizeof(int));
 	}
+    vycisti_pole(inst->pole, inst->velkost_strany);
 	return inst;
 }
  
@@ -22,9 +26,17 @@ size_t up_matica_daj_velkost (Matica* inst)
 }
 
 void up_matica_zrus(Matica* inst){
-	for (int i = 0; i < inst->velkost_strany; i++) {
+	for (size_t i = 0; i < inst->velkost_strany; i++) {
 		free ((void *) inst->pole[i]);
 	}
 	free ((void *) inst->pole);
 	free((void *) inst);
+}
+
+static void vycisti_pole(int **pole, size_t velkost) {
+    for (size_t i = 0; i < velkost; i++) {
+        for (size_t j = 0; j < velkost; j++) {
+            pole[i][j] = 0;
+        }
+    }
 }
