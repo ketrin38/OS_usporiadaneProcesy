127b71cdbe01fe699e38c714c0999e855ea3f776
ketrin38 <ketrin.pilarcik@gmail.com>
2016-12-19 14:43:04 +0100

    Skuska semaforov ide spravne.

--- skuska.c
+++ skuska.c
@@ -3,125 +3,67 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-#include <stdio.h>
 #include <errno.h>
-#include <unistd.h>
+
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h> 
-#include <sys/ipc.h>
-#include <fcntl.h>
-#include <sys/sem.h>
-#include <semaphore.h>
-
-#include "Parser.h"
-#include "App.h"
-#include "Front.h"
-#include "Matica.h"
-
-typedef void (*proc_funkcia_t)(void);
-
-typedef struct _Proces {
-	sem_t * semafor;
-	proc_funkcia_t proc_funkcia;
-} Proces;
 
 
-static void nacitaj_procesy(App* inst, const Matica* matica, int prvy_proc);
-static Proces* proces_vytvor(sem_t* semafor, void* proces_funkcia);
-static void proces_vykonaj(Proces* proces);
-static void funkcia_1();
-static void funkcia_2();
-static void funkcia_3();
-static void funkcia_4();
-static void funkcia_5();
+static void procesA(sem_t* sem_b, sem_t* sem_c);
+static void procesB(sem_t* sem_b, sem_t* sem_a);
+static void procesC(sem_t* sem_d, sem_t* sem_c);
+static void procesD(sem_t* sem_d, sem_t* sem_a);
 
+int main () {
+	printf("Vytvaram semafory  ...... \n");
+	sem_t *sem_a = sem_open("test_a", O_CREAT|O_EXCL, 0644, 0);
+    sem_t *sem_b= sem_open("test_b", O_CREAT|O_EXCL, 0644, 0);
+	sem_t *sem_c= sem_open("test_c", O_CREAT|O_EXCL, 0644, 0);
+	sem_t *sem_d= sem_open("test_d", O_CREAT|O_EXCL, 0644, 0);
+	printf("Inicializujem semafory  ...... \n");
+	sem_unlink("test_a"); // INICIALIZOVAT!
+    sem_unlink("test_b");
+    sem_unlink("test_c"); // INICIALIZOVAT!
+    sem_unlink("test_d");
 
-	sem_t* poleSemaforov[velkost_matice];
-	sem_t *katka = sem_open("test_1", O_CREAT|O_EXCL, 0, 1);
-    sem_unlink("test_1"); // INICIALIZOVAT!
-	sem_t *miska = sem_open("test_2", O_CREAT|O_EXCL, 0, 1);
-    sem_unlink("test_2");
-	sem_init(katka,1,1);
-	sem_init(miska,1,1);
-	poleSemaforov[0]= katka;
-	poleSemaforov[1]= katka;
-	poleSemaforov[2]= katka;
-	poleSemaforov[3]= miska;
-	poleSemaforov[4]= miska;
+	sem_init(sem_a,0,0);
+	sem_init(sem_b,0,0);
+	sem_init(sem_c,0,0);
+	sem_init(sem_d,0,0);
 	
-	proc_funkcia_t funkcie[5] = {
-		funkcia_1,
-		funkcia_2,
-		funkcia_3,
-		funkcia_4,
-		funkcia_5
-	};
+	procesA(sem_b,sem_c);
+	procesB(sem_b,sem_a);
+	procesC(sem_d,sem_c);
+	procesD(sem_d,sem_a);
 	
-	memset(poleSemaforov, 0, velkost_matice * sizeof(sem_t*));
-	while (!up_front_je_prazdny(front)) {
-		int aktualny = up_front_pop(front);
-		// Prejde cez vsetky incidentne vrcholy
-		int poc_inc_vrcholov = 0;
-		for (size_t j = 0; j < velkost_matice; j++) {
-			if (pole[aktualny][j] == 1) {
-				poc_inc_vrcholov++;
-				sem_t* semafor = poleSemaforov[aktualny];
-				//sem_t* semafor; // tu treba menit nazov semafora   !!!!!!!
-				// INIT bude asi az potom ***********************
-				Proces* proces = proces_vytvor(semafor, funkcie[aktualny]);
-				pid_t fork_vysledok = fork();
-				if (fork_vysledok == 0) { // Potomok
-					printf("After fork....\n");
-					proces_vykonaj(proces);
-					printf("PO fork....\n");
-					return;
-				} else if (fork_vysledok < 0) {
-					// Chyba
-				}
-				up_front_push(front, j);
-			}
-		}
-	}
-	up_front_zrus(front);
+	printf("Skoncil som sa .... \n");
+	sem_destroy(sem_a);
+	sem_destroy(sem_b);
+	sem_destroy(sem_c);
+	sem_destroy(sem_d);
+	printf("zmazal som semafory .... \n");
 } 
 
-static Proces* proces_vytvor(sem_t* semafor, void* proces_funkcia){
-	Proces* inst = (Proces*)malloc(sizeof(Proces));
-	inst->semafor = semafor;
-	inst->proc_funkcia = proces_funkcia;
-	return inst;
-}
 
-static void proces_vykonaj(Proces* proces) {
-	while(1) {
-		printf("vo WHILEEE\n");
-		int hodnota;
-		sem_getvalue(proces->semafor, &hodnota);
-	    printf("pred waitom, semafor hodnota = %d\n", hodnota);
-		sem_wait(proces->semafor);
-		printf("HERE 1\n");
-		proces->proc_funkcia();
-		sem_post(proces->semafor);
-	}
+static void procesA(sem_t* sem_b, sem_t* sem_c) {
+	printf("Som A\n");
+	sem_post(sem_b);
+	sem_post(sem_c);
 } 
-
-static void funkcia_1() {
-	printf("1\n");
-}
-
-static void funkcia_2() {
-	printf("2\n");
-}
-static void funkcia_3() {
-	printf("3\n");
-}
-static void funkcia_4() {
-	printf("4\n");
-}
-static void funkcia_5() {
-	printf("5\n");
-}
-
-int main () {} 
-
+static void procesB(sem_t* sem_b, sem_t* sem_a) {
+	sem_wait(sem_b);
+	printf("Som B\n");
+	sem_post(sem_a);
+} 
+static void procesC(sem_t* sem_d, sem_t* sem_c) {
+	sem_wait(sem_c);
+	printf("Som C\n");
+	sem_post(sem_d);
+} 
+static void procesD(sem_t* sem_d, sem_t* sem_a) {
+	sem_wait(sem_a);
+	printf("Som pred D\n");
+	sem_wait(sem_d);
+	printf("Som D\n");
+} 
